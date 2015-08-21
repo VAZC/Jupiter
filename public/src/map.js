603f5832df9b9d0469e22387fafb6600d12b423a
lexamo <amobe.momoko@gmail.com>
2015-08-21 20:01:18 +0800

    Set your location on when page loaded, if cannot get your location, then set center location at Kaohsiung.

--- public/src/map.js
+++ public/src/map.js
@@ -1,11 +1,48 @@
 var map;
+var myLocationMarker;
+var defaultCenterLatLng = {
+    lat: 22.38,
+    lng: 120.17
+};
 
-function initMap() {
+function main() {
+    console.log("centerLatLng");
+    getLocation();
+    initMap();
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(initMap, getLocationFail);
+    } else {
+        console.log("Geolocation is not support at this browser.");
+    }
+}
+
+function getLocationFail(error) {
+    console.log(error);
+    initMap(null);
+}
+
+function initMap(position) {
+    var centerLatLng;
+    if (position) {
+        centerLatLng = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        }
+    } else {
+        centerLatLng = defaultCenterLatLng;
+    }
+
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 25.1348,
-            lng: 121.7321
-        },
-        zoom: 8
+        center: centerLatLng,
+        zoom: 13
+    });
+
+    myLocationMarker = new google.maps.Marker({
+        position: centerLatLng,
+        animation: google.maps.Animation.DROP,
+        map: map
     });
 }
