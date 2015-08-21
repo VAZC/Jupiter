1a91b6e99a7b722c3df5c3801340b5d3c0571630
Vantist <s7790428@gmail.com>
2015-08-22 02:57:59 +0800

    add cross line in map

--- public/src/map.js
+++ public/src/map.js
@@ -1,5 +1,6 @@
 var map;
 var myLocationMarker;
+// var jupiter;
 var defaultCenterLatLng = {
     lat: 25.047833423462535,
     lng: 121.51709318161011
@@ -55,11 +56,91 @@ function initMap(position) {
             scale: 8
         }
     });
+
     $.get('/json', function(result) {
+        var stations = result;
+        var stationsfc = [];
         for (var i = 0; i < result.length; i++) {
             console.log(result[i]);
             var point = turf.point([result[i].lon, result[i].lat]);
+            stations[i].point = point;
+            stationsfc.push(point);
             map.data.addGeoJson(point);
         };
+
+        var tin = turf.tin(turf.featurecollection(stationsfc), 'elevation');
+        console.log(tin);
+        map.data.addGeoJson(tin);
+
+        // jupiter.setStations(stations);
+        // jupiter.calcBound();
     });
 }
+
+// function Jupiter() {
+//     // private
+//     var that = this;
+//     var stations;
+
+//     // public
+//     this.setStations = setStations;
+//     this.getStations = getStations;
+//     this.getFilterGeoStations = getFilterGeoStations;
+//     this.calcBound = calcBound;
+
+//     function setStations(s) {
+//         stations = s;
+//     }
+
+//     function getStations() {
+//         return stations;
+//     }
+
+//     function getGeoStations(station) {
+//         var geoStations = [];
+
+//         for (var i = 0; i < stations.length; i++) {
+//             if (!station && station === stations[i])
+//                 continue;
+
+//             geoStations.push(stations[i].point);
+//         }
+
+//         return geoStations;
+//     }
+
+//     function getFilterGeoStations(station) {
+//         return getGeoStations(stations.filter(function(obj) {
+//             return obj !== station;
+//         }));
+//     }
+
+//     function calcBound() {
+//         for (var i = 0; i < stations.length; i++) {
+//             var filterGeoStations = getFilterGeoStations();
+//             var current = stations[i];
+//             current.nextpoint = turf.nearest(current.point, filterGeoStations);
+//         }
+
+        // for (var i = 0; i < stations.length; i++) {
+        //     var current = stations[i];
+        //     var current.nextpoint = current;
+        //     current.nextpoint.dis = Number.MAX_SAFE_INTEGER;
+
+        //     for (var j = 0; j < station.length; j++) {
+        //         if (i === j) continue;
+        //         var nextpoint = stations[j];
+
+        //         var dis = turf.distance(current.point, nextpoint.point, 'kilometers');
+        //         if (dis < current.nextpoint.dis) {
+        //             current.nextpoint = nextpoint;
+        //             current.nextpoint.dis = dis;
+        //         }
+        //     }
+        // }
+//     }
+
+//     function paintBoundArea() {
+
+//     }
+// }
