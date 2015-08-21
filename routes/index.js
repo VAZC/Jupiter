edb3ca9ff3099384c5224f2a6ddf9720baa0fc1d
Chrono <x941i6c04@gmail.com>
2015-08-21 18:20:29 +0800

    xml 2 json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var request = require('request');
+// var cheerio = require('cheerio');
+var parser = require('xml2json');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express'
+    });
+});
+
+router.get('/json', function(req, res, next) {
+    var options = {
+        url: 'http://opendata.cwb.gov.tw/member/opendataapi?dataid=O-A0002-001&authorizationkey=CWB-24564629-A5A6-409B-A91D-7996423BF593',
+        headers: {
+            'User-Agent': 'request'
+        }
+    };
+    request(options, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var json = parser.toJson(body);
+            res.json(json);
+        }
+    })
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/data/json', function(req, res, next) {
   res.send('respond with a resource');
+  var http = require('http');
+  var fs = require('fs');
+
+  var file = fs.createWriteStream("file.jpg");
+  var request = http.get("http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg", function(response) {
+	    response.pipe(file);
+  });
 });
 
 module.exports = router;
