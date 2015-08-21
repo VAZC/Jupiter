3d5be4946483048174b10d478a24662fddc85387
Chrono <x941i6c04@gmail.com>
2015-08-21 22:46:34 +0800

    show stations on map

--- public/src/map.js
+++ public/src/map.js
@@ -1,14 +1,13 @@
 var map;
 var myLocationMarker;
 var defaultCenterLatLng = {
-    lat: 22.38,
-    lng: 120.17
+    lat: 25.047833423462535,
+    lng: 121.51709318161011
 };
 
 function main() {
     console.log("centerLatLng");
     getLocation();
-    initMap();
 }
 
 function getLocation() {
@@ -44,5 +43,23 @@ function initMap(position) {
         position: centerLatLng,
         animation: google.maps.Animation.DROP,
         map: map
+    });
+
+    map.data.setStyle({
+        icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            fillColor: '#333333',
+            fillOpacity: 0.8,
+            strokeColor: '#666666',
+            strokeOpacity: 0.5,
+            scale: 8
+        }
+    });
+    $.get('/json', function(result) {
+        for (var i = 0; i < result.length; i++) {
+            console.log(result[i]);
+            var point = turf.point([result[i].lon, result[i].lat]);
+            map.data.addGeoJson(point);
+        };
     });
 }
