ac8ff5502cfc4b794d4fcb708179ebb107b6ee81
Chrono <x941i6c04@gmail.com>
2015-08-21 18:48:41 +0800

    fix output json

--- routes/index.js
+++ routes/index.js
@@ -13,16 +13,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/json', function(req, res, next) {
+    res.header('Content-Type', 'application/json; charset=utf-8');
     var options = {
-        url: 'http://opendata.cwb.gov.tw/member/opendataapi?dataid=O-A0002-001&authorizationkey=CWB-24564629-A5A6-409B-A91D-7996423BF593',
-        headers: {
-            'User-Agent': 'request'
-        }
+        url: 'http://opendata.cwb.gov.tw/member/opendataapi?dataid=O-A0002-001&authorizationkey=CWB-24564629-A5A6-409B-A91D-7996423BF593'
     };
     request(options, function(error, response, body) {
         if (!error && response.statusCode == 200) {
             var json = parser.toJson(body);
-            res.json(json);
+            res.send(json);
         }
     })
 });
