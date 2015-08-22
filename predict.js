428dbec8eb01e3f579d9a51d04231b1921be2dee
Zam <love8879201@gmail.com>
2015-08-23 03:22:48 +0800

    update to match station api

--- predict.js
+++ predict.js
@@ -1,95 +1,48 @@
 'use strict';
 var turf = require('turf'),
-	extend = require('util')._extend;
-
-var cellList = [
-{	// object 1
-	lat : 25,	// center lat
-	lon : 120,	// center lon
-	neighbors : [
-		// object2 reference, 
-		// object3 reference, 
-		// object4 reference, 
-		// object5 reference, 
-		// object6 reference
-	],
-},
-{	// object 2
-	lat : 25.1,
-	lon : 120,
-
-	neighbors : [
-		// object1 reference, 
-		// object3 reference, 
-		// object4 reference, 
-		// object5 reference, 
-		// object6 reference
-	],
-}];
-
-var predict = {
-	// get possible area
-	possibleAreas : function(oldRainyAreas, newRainyAreas) {
-		var possibleAreas = [];
-
-		// extract different area
-		newRainyAreas = this.filter(oldRainyAreas, newRainyAreas);
-		// get possible areas
-		oldRainyAreas.forEach(function(oldRainyArea) {
-			newRainyAreas.forEach(function(newRainyArea) {
-				// find neighbors
-				oldRainyArea.neighbors.forEach(function(neighborArea) {
-					if (this.isSameArea(neighborArea, newRainyArea)) {
-						var areas = find(oldRainyArea, newRainyArea);
-						possibleAreas.push.apply(possibleAreas, areas);
-					}
-				});
-			});
-		});
-		return possibleAreas;
-	},
 	// find possible area
-	find : function(oldRainyArea, newRainyArea) {
-		var start = turf.point([oldRainyArea.lon, oldRainyArea.lat]),
-			end = turf.point([newRainyArea.lon, newRainyArea.lat]),
-			geoInfo = geoInfo(start, end),
+	find = function(oldRainyArea, newRainyArea) {
+		var start = turf.point([oldRainyArea.getLon(), oldRainyArea.getLat()]),
+			end = turf.point([newRainyArea.getLon(), newRainyArea.getLat()]),
+			geoInfo = getGeoInfo(start, end),
 			areas = [];
 			// find possible area
-			newRainyArea.neighbors.forEach(function(neighborArea) {
-				if (this.isPossibleArea(newRainyArea, neighborArea, geoInfo)) {
+			newRainyArea.getNeibors().forEach(function(neighborArea) {
+				if (isPossibleArea(newRainyArea, neighborArea, geoInfo)) {
 					areas.psuh(neighborArea);
 				}
 			});
 		return areas;
 	},
 	// return different area
-	filter : function(oldRainyAreas, newRainyAreas) {
+	filter = function(oldRainyAreas, newRainyAreas) {
 		return newRainyAreas.filter(function(newRainyArea) {
 			var result = true;
 			// filter different
 			oldRainyAreas.forEach(function(oldRainyArea) {	
-				result = !this.isSameArea(oldRainyArea, newRainyArea);
+				result = !isSameArea(oldRainyArea, newRainyArea);
 			});
 			return result;
 		});
 	},
 	// check same area
-	isSameArea : function(area1, area2) {
-		if (area1.lat === area2.lat && area1.lon === area2.lon) {
+	isSameArea = function(area1, area2) {
+		if (area1.getLat() === area2.getLat() && area1.getLon() === area2.getLon()) {
 			return true;
 		}
 		return false;
 	},
 	// check possible area
-	isPossibleArea : function(area, neighborArea, geoInfo) {
-		var start = turf.point([area.lon, area.lat]),
-			end = turf.point([neighborArea.lon, neighborArea.lat]),
-			neighborGeoInfo = geoInfo(start, end),
+	isPossibleArea = function(area, neighborArea, geoInfo) {
+		var start = turf.point([area.getLon(), area.getLat()]),
+			end = turf.point([neighborArea.getLon(), neighborArea.getLat()]),
+			neighborGeoInfo = getGeoInfo(start, end),
 			distanceUnit = 0.1,
-			distance = distanceUnit;
+			distance = distanceUnit,
+			i = 1;
 		// use increasing distance to find out passby area
-		while (distance >= geoInfo.distance) {
-			distance = i * idistanceUnit;
+		while (distance <= neighborGeoInfo.distance) {
+			distance = i++ * distanceUnit;
 			var point = turf.destination(start, distance, geoInfo.bearing, 'kilometers');
 			if (neighborArea.contains(point)) {
 				return true;
@@ -98,12 +51,32 @@ var predict = {
 		return false;
 	},
 	// get two location geo info
-	geoInfo : function(start, end) {
+	getGeoInfo = function(start, end) {
 		return {
 			distance : turf.distance(start, end),
 			bearing : turf.bearing(start, end)
-		}
+		};
+	};
+
+module.exports = {
+	// get possible area
+	possibleAreas : function(oldRainyAreas, newRainyAreas) {
+		var possibleAreas = [];
+
+		// extract different area
+		newRainyAreas = filter(oldRainyAreas, newRainyAreas);
+		// get possible areas
+		oldRainyAreas.forEach(function(oldRainyArea) {
+			newRainyAreas.forEach(function(newRainyArea) {
+				// find neighbors
+				oldRainyArea.getNeibors().forEach(function(neighborArea) {
+					if (isSameArea(neighborArea, newRainyArea)) {
+						var areas = find(oldRainyArea, newRainyArea);
+						possibleAreas.push.apply(possibleAreas, areas);
+					}
+				});
+			});
+		});
+		return possibleAreas;
 	}
 };
-
-module.exports = predict;
