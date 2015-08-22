e8361d02203385525b8a809035c0347e4d64a2cf
Vantist <s7790428@gmail.com>
2015-08-23 01:52:05 +0800

    add station object

--- station.js
+++ station.js
@@ -1,22 +1,39 @@
-var sqlite3 = require("sqlite3").verbose();
-var db = new sqlite3.Database('./tenrain.sqlite');
+var d3 = require('d3');
+var turf = require('turf');
+var stationModel = require('./model/station.js');
 
-var StationModel = function(callback) {
-    db.serialize(function() {
-        var select_station_query = db.prepare("\
-        	select\
-        		station.*,\
-        		(select min_10 from measurement where measurement.stationId=station.stationId order by measurement.time desc limit 1) as now_10,\
-        		(select min_10 from measurement where measurement.stationId=station.stationId order by measurement.time desc limit 1 offset 1) as last_10\
-        	from station");
-        select_station_query.all([], function(err, rows) {
-            callback(rows);
-        });
-    });
-};
+var data = [[0,0], [100,100], [200,200]];
+var voronoi = d3.geom.voronoi();
+var polygons = voronoi(data);
 
-StationModel(function(list) {
-    console.log(list);
+console.log(turf.point([123, 456]))
+
+stationModel(function(data) {
+	console.log(data);
 });
 
-module.exports = StationModel;
+function Station(s) {
+	// private
+	var name = s.locationName;
+	var lat = s.lat;
+	var lon = s.lon;
+	var city = s.city;
+	var belong = s.attribute;
+	var rain = {
+		now: s.now_10,
+		old: s.last_10
+	}
+	var point = [lat, lon];
+	var geo_point = turf.point(point);
+	var neibor = [];
+
+	// public
+	this.getName = function() { return name; }
+	this.getPoint = function() { return point; }
+	this.getGeoPoint = function() { return geo_point; }
+	this.getCity = function() { return city; }
+	this.getBelong = function() { return belong; }
+	this.getRain = function() { return JSON.parse(JSON.stringify(rain)); }
+	this.getNeibor = function() { return JSON.parse(JSON.stringify(neibor)); }
+	this.setNeibor = function(n) { neibor = n; }
+}
